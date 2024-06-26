a401b41d5355fd676465c5b91ccc345996a05b41
ley roy <atiwensolomon@gmail.com>
2024-06-26 06:25:43 -1200

    first commit

---
+++ todo.js
@@ -0,0 +1,23 @@
+
+const [counter, setCounter] = useState(0)
+
+setCounter(++counter)
+sorre:{
+    user:{}
+    loading:false
+
+    reducer:{
+        setUwer:(state, aciton)=>{
+                
+        }
+        isLoading:(state)=>{
+                state.loading=true
+        }
+    }
+}
+
+export const user = ({})=>{
+        <div>{prop.name}</div>
+}
+
+setUwer(user)
